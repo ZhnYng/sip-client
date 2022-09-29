cdd39365a2f73f55b155550b503e8335c1f06bc7
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2022-09-29 11:59:50 +0800

    make it better

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000';
-// axios.defaults.baseURL = 'https://sipappdev.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://sipappdev.herokuapp.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
